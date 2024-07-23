efff45cc1d9dad54d4a153da0f7ea3dd2a4a354a
Sartios Savramis <savramis.sartios@gmail.com>
2024-07-23 18:21:28 +0300

    Create task execution role and policies

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -5,3 +5,45 @@
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
+
+data "aws_iam_policy_document" "task_execution_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "task_execution_role_policy" {
+  name        = "${local.prefix}-task-execution-role-policy"
+  description = "Allow ECS to retrieve images and add to logs."
+  policy      = data.aws_iam_policy_document.task_execution_role_policy.json
+}
+
+data "aws_iam_policy_document" "task_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${local.prefix}-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.task_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = aws_iam_policy.task_execution_role_policy.arn
+}
